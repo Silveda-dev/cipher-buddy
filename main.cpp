29874d243748909af9c509331372daeaca17ca8f
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-05-26 00:40:35 +0930

    Get input and template later flow

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::string encrypted;
+    
+    std::cout << "Welcome to Cipher Buddy.\nEnter text to be decrypted: " << std::endl;
+    std::cin >> encrypted;
+
+    //Call entropy calculator
+
+    //Call appropriate cipher algorithms
+
+    return 0;
+}
